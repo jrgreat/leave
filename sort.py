634092b23d96f0e171598e9ad01408f9c90ff8b0
jrgreat <37722881+jrgreat@users.noreply.github.com>
2021-12-25 22:17:52 +0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -9,8 +9,26 @@ def bubble_sort(to_be_sort_list):
 
     print(to_be_sort_list)
 
+def quick_sort(data):
+    if len(data) >= 2:
+        mid = data[len(data)//2]
+        left = list()
+        right = list()
+        data.remove(mid)
+        for num in data:
+            if num >= mid:
+                right.append(num)
+            else:
+                left.append(num)
+        return quick_sort(left) + [mid] + quick_sort(right)
+    else:
+        return data
+
+
 
 if __name__=="__main__":
     the_list = [6,2,3,8,0,1,9,7]
-    bubble_sort(the_list)
+    #bubble_sort(the_list)
+    print(quick_sort(the_list))
+
 
